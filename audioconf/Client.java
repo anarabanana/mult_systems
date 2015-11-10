7c4eca28bfda4fff5eb5ee34030ceabd37478421
nut-hatch <niklas.kolbe@gmail.com>
2015-11-10 22:29:52 +0100

    Change to java-way of Pipe creation

--- audioconf/Client.java
+++ audioconf/Client.java
@@ -1,40 +1,68 @@
 package audioconf;
 
+import org.gstreamer.Caps;
 import org.gstreamer.Element;
 import org.gstreamer.ElementFactory;
 import org.gstreamer.Gst;
 import org.gstreamer.Pipeline;
 import org.gstreamer.State;
-import org.gstreamer.elements.PlayBin2;
 
+public class Client {
 
-	public class Client {
-	
-	static Pipeline audiopipe;
-	
-	static String MULTICAST_IP_ADDR = "224.1.1.1";
-	static int AUDIO_UDP_PORT = 3000;
-	
-	public static void main(String[] args) 
-{
-		String [] rec = new String[]{"udpsrc port = 5001", "!", "application/x-rtp, media=(string)audio, " +
-				"clock-rate=44100, width=16, height=16, encoding-name=(string)L16, encoding-params=(string)1," +
-				" channels=(int)1, channel-position=(int)1, payload=(int)96" ,"!","gstrtpjitterbuffer do-lost=true", 
-				"!" , "rtpL16depay", "!", "audioconvert", "!", "alsasink"};
-		 
-		
-	rec = Gst.init("Client", rec); 
-	StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) {
+		args = Gst.init("AudioPipe", args);
 
-	for (String s: rec) {
-		 sb.append(' ');
-		 sb.append(s);  
-		 				}
-	
-		audiopipe = Pipeline.launch(sb.substring(1));	
-		audiopipe.setState(State.PLAYING);
+		Pipeline pipe = new Pipeline("AudioPipe");
+
+		// Create elements.
+		Element src = ElementFactory.make("udpsrc", "Source");
+		src.set("port", ServerEdited.udp_port);
+
+		String xrtp = "application/x-rtp, media=audio, clock-rate=44100, width=16, height=16, channel=1, channel-position=1, payload=96, encoding-name=L16, encoding-params=1";
+		src.getSrcPads().get(0).setCaps(Caps.fromString(xrtp));
+
+		Element buffer = ElementFactory.make("gstrtpjitterbuffer", "Buffer");
+		buffer.set("do-lost", true);
+
+		Element depay = ElementFactory.make("rtpL16depay", "Depay");
+
+		Element convert = ElementFactory.make("audioconvert", "Convert");
+
+		Element sink = ElementFactory.make("alsasink", "Destination");
+
+		// Create pipeline.
+		pipe.addMany(src, buffer, depay, convert, sink);
+		src.link(buffer, depay, convert, sink);
+
+		// Start
+		pipe.setState(State.PLAYING);
 		Gst.main();
-		audiopipe.setState(State.NULL);
+		pipe.setState(State.NULL);
 
-}
+		// String[] rec = new String[] {
+		// "udpsrc port = 5002",
+		// "!",
+		// "application/x-rtp, media=(string)audio, "
+		// +
+		// "clock-rate=44100, width=16, height=16, encoding-name=(string)L16, encoding-params=(string)1,"
+		// + " channels=(int)1, channel-position=(int)1, payload=(int)96", "!",
+		// "gstrtpjitterbuffer do-lost=true", "!", "rtpL16depay",
+		// "!", "audioconvert", "!", "alsasink" };
+
+		// rec = Gst.init("Client", rec);
+		//
+		// StringBuilder sb = new StringBuilder();
+		//
+		// for (String s : rec) {
+		// sb.append(' ');
+		// sb.append(s);
+		// }
+
+		// audiopipe = Pipeline.launch(sb.substring(1));
+		// pipe.setState(State.PLAYING);
+		// Gst.main();
+		// pipe.setState(State.NULL);
+
+	}
+
 }
