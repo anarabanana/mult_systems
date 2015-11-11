bb670383efc63f15d229a116638c53b598e66d33
Anarabanana <bazarhanova@gmail.com>
2015-11-11 14:38:07 +0100

    Removed ServerEdited which was so irritating Niklas

--- audioconf/Server.java
+++ audioconf/Server.java
@@ -1,5 +1,56 @@
 package audioconf;
 
+import org.gstreamer.Caps;
+import org.gstreamer.Element;
+import org.gstreamer.ElementFactory;
+import org.gstreamer.Gst;
+import org.gstreamer.Pad;
+import org.gstreamer.Pipeline;
+
+/**
+ *
+ */
+public class Server {
+
+	public static String udp_ip = "localhost";
+	public static int udp_port = 5001;
+
+	public Server() {
+	}
+
+	public static void main(String[] args) {
+
+		args = Gst.init("ServerEdited", args);
+
+		/* create elements */
+		Pipeline pipeline = new Pipeline("send_pipeline");
+
+		Element source = ElementFactory.make("alsasrc", "alsasrc");
+		Element audioconvert = ElementFactory.make("audioconvert", "audioconvert");
+
+		Element caps = ElementFactory.make("capsfilter", "caps");
+
+		caps.setCaps(Caps.fromString("audio/x-raw-int,channels=1,depth=16,width=16,rate=44100"));
+
+		Element rtpPay = ElementFactory.make("rtpL16pay", "rtpL16pay");
+
+		Element sink = ElementFactory.make("udpsink", "udpsink");
+		sink.set("host", udp_ip);
+		sink.set("port", udp_port);
+
+		/* put together a pipeline */
+		pipeline.addMany(source, audioconvert, caps, rtpPay, sink);
+		Element.linkMany(source, audioconvert, caps, rtpPay, sink);
+
+		/* start the pipeline */
+		pipeline.play();
+		Gst.main();
+		pipeline.stop();
+	}
+}
+
+/*package audioconf;
+
 import org.gstreamer.Element;
 import org.gstreamer.ElementFactory;
 import org.gstreamer.Gst;
@@ -36,3 +87,4 @@ public class Server {
 
 	}
 }
+*/
